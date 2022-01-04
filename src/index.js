01726223f2de6a17639510a127749de4b2e39870
amarynets <andrii.marynets@gmail.com>
2022-01-04 21:14:10 +0200

    Temp commit with \n

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import readlineSync from 'readline-sync';
 
 const engine = (rules, raund) => {
   const numberOfRounds = 3;
-  console.log('Welcome to the Brain Games!');
+  console.log('Welcome to the Brain Games!\n');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}`);
-  console.log(rules);
+  console.log(`\n ${rules}\n`);
   for (let i = 0; i < numberOfRounds; i += 1) {
     const [question, correctAnswer] = raund();
-    console.log(`Question: ${question}`);
+    console.log(`Question: ${question}\n`);
     const answer = readlineSync.question('Your answer: ');
     if (answer === correctAnswer) {
-      console.log('Correct!');
+      console.log('\nCorrect!\n');
     } else {
-      const msg = `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`;
+      const msg = `'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!\n`;
       return console.log(msg);
     }
   }
-  return console.log(`Congratulation, ${playerName}!`);
+  return console.log(`Congratulation, ${playerName}!\n`);
 };
 
 const generateNumber = (maxNumber) => Math.floor(Math.random() * maxNumber);
